1aff561643274d98d37b168bc698afb5816f20de
Salmaan Saeed <salsaeed999@gmail.com>
2023-08-25 18:43:56 -0400

    deleted the old frontend

--- frontend_backup/src/Redirect.js
+++
@@ -1,6 +0,0 @@
-export default function Redirect(route_name) {
-    /* Redirect and Reload the redirected page */
-    const route = "#" + route_name;
-    location.href = route;
-    location.reload(true);
-}
--- frontend_backup/src/components/pages/PageNotFound.vue
+++
@@ -1,4 +0,0 @@
-<template>
-    <h1>404</h1>
-    <p>Page Not Found</p>
-</template>
