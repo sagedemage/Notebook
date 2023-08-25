eec6a8c24f438d7e8d73cf66f769924a80108d38
Salmaan Saeed <salsaeed999@gmail.com>
2023-08-25 19:45:06 -0400

    added a bootstrap naviation bar to the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,33 +5,30 @@ import About from './components/pages/About'
 import Login from './components/pages/Login'
 import Register from './components/pages/Register'
 import Dashboard from './components/pages/Dashboard'
+import Navigationbar from './components/ui/Navigationbar'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
-//import './App.css'
+import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/login">Login</a></li>
-        <li><a href="/register">Register</a></li>
-        <li><a href="/dashboard">Dashboard</a></li>
-      </ul>
-      <BrowserRouter>
-        <Suspense fallback="Loading...">
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="about" element={<About />}></Route>
-            <Route path="login" element={<Login />}></Route>
-            <Route path="register" element={<Register />}></Route>
-            <Route path="dashboard" element={<Dashboard />}></Route>
-          </Routes>
-        </Suspense>
-      </BrowserRouter>
+    <div id="body">
+      <Navigationbar />
+      <main id="content">
+        <BrowserRouter>
+          <Suspense fallback="Loading...">
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="about" element={<About />}></Route>
+              <Route path="login" element={<Login />}></Route>
+              <Route path="register" element={<Register />}></Route>
+              <Route path="dashboard" element={<Dashboard />}></Route>
+            </Routes>
+          </Suspense>
+        </BrowserRouter>
+      </main>
     </div>
   )
 }
