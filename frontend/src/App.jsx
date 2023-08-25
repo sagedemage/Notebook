96479a8103bb2e31c9cec10268aad5e0ece1a4d6
Salmaan Saeed <salsaeed999@gmail.com>
2023-08-25 19:14:58 -0400

    setup the router using react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, Suspense, lazy } from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+//import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+      </ul>
+      <BrowserRouter>
+        <Suspense fallback="Loading...">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="about" element={<About />}></Route>
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </div>
   )
 }
 
