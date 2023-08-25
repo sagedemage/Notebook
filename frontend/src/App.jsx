c4787a73cc5528eb3934edca04d74e652c4fe6dd
Salmaan Saeed <salsaeed999@gmail.com>
2023-08-25 19:20:18 -0400

    added blank login, register, and dashboard pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import { useState, Suspense, lazy } from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from './components/pages/Home'
 import About from './components/pages/About'
+import Login from './components/pages/Login'
+import Register from './components/pages/Register'
+import Dashboard from './components/pages/Dashboard'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 //import './App.css'
@@ -14,12 +17,18 @@ function App() {
       <ul>
         <li><a href="/">Home</a></li>
         <li><a href="/about">About</a></li>
+        <li><a href="/login">Login</a></li>
+        <li><a href="/register">Register</a></li>
+        <li><a href="/dashboard">Dashboard</a></li>
       </ul>
       <BrowserRouter>
         <Suspense fallback="Loading...">
           <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="about" element={<About />}></Route>
+            <Route path="login" element={<Login />}></Route>
+            <Route path="register" element={<Register />}></Route>
+            <Route path="dashboard" element={<Dashboard />}></Route>
           </Routes>
         </Suspense>
       </BrowserRouter>
---
+++ frontend/src/components/pages/Login.jsx
@@ -0,0 +1,10 @@
+/* Login Page */
+
+export default function Login() {
+    return (
+        <>
+            <h1>Login</h1>
+            <p>This is the login page.</p>
+        </>
+    )
+}
