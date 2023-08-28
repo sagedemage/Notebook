3d639b68ea6922e813be90af13a4c262e7851b0e
Salmaan Saeed <salsaeed999@gmail.com>
2023-08-28 18:25:17 -0400

    made navbar render auth or non auth links if the user logged in or not

--- frontend/src/components/ui/NavigationBar.jsx
+++ frontend/src/components/ui/NavigationBar.jsx
@@ -1,9 +1,36 @@
 /* Navigation Bar */
+import { useEffect, useState } from 'react'
 import { Container } from 'react-bootstrap'
 import { Nav } from 'react-bootstrap'
 import { Navbar } from 'react-bootstrap'
+import Cookies from 'universal-cookie'
+import axios from 'axios'
 
 export default function NavigationBar() {
+    const [isAuth, setAuth] = useState(false)
+    useEffect(() => {
+        const cookies = new Cookies()
+        const token = cookies.get("token")
+        
+        if (token !== undefined) {
+            axios.post("http://localhost:8000/api/get-decoded-token", {
+                token: token,
+            }).then((response) => {
+                if (response.data.auth === true) {
+                    setAuth(true)
+                }
+                else {
+                    setAuth(false)
+                }
+            }).catch(err => {
+                console.log(err)
+            })
+        }
+        else {
+            setAuth(false)
+        }
+    }, [])
+
     return (
         <>
             <Navbar expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark" fixed="top">
@@ -14,10 +41,20 @@ export default function NavigationBar() {
                         <Nav className="me-auto">
                             <Nav.Link href="/">Home</Nav.Link>
                             <Nav.Link href="/about">About</Nav.Link>
-                            <Nav.Link href="/login">Login</Nav.Link>
-                            <Nav.Link href="/register">Register</Nav.Link>
-                            <Nav.Link href="/dashboard">Dashboard</Nav.Link>
                         </Nav>
+                        {/* Non Auth Links */}
+                        {isAuth === false &&
+                            <Nav className='ms-auto'>
+                                <Nav.Link href="/login">Login</Nav.Link>
+                                <Nav.Link href="/register">Register</Nav.Link>
+                            </Nav>
+                        }
+                        {/* Auth Links */}
+                        { isAuth === true &&
+                            <Nav className='ms-auto'>
+                                <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+                            </Nav>
+                        }
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
