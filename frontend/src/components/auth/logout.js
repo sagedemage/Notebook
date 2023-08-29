9a8aeb90ed79e632447246a60192ae1ce2e5eeee
Salmaan Saeed <salsaeed999@gmail.com>
2023-08-29 17:00:48 -0400

    removed unused comment

--- frontend/src/components/auth/logout.js
+++ frontend/src/components/auth/logout.js
@@ -3,7 +3,6 @@ import Cookies from 'universal-cookie'
 export const Logout = () => {
     /* Logout Action */
     const cookies = new Cookies()
-    //const navigate = useNavigate();
 
     // remove cookie token
     cookies.remove('token')
