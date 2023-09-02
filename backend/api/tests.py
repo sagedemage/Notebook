91b68f0eaf95d1d8355d357328ff4f8082e43c9e
Salmaan Saeed <salsaeed999@gmail.com>
2023-09-01 23:54:15 -0400

    wrote test login success and made client a class variable

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -7,14 +7,21 @@ from rest_framework.test import APIClient
 
 
 class Auth(TestCase):
+    client = APIClient()
     def test_register_success(self):
-        client = APIClient()
-        response = client.post('/api/register', {'email': 'test1000@email.com', 'username': 'test1000', 'password': 'test1000'}, format='json')
+        response = self.client.post('/api/register', {'email': 'test1000@email.com', 'username': 'test1000', 'password': 'test1000'}, format='json')
         self.assertEquals(response.status_code, status.HTTP_200_OK)
         self.assertEquals(response.content, b'{"registered": true, "success_msg": "User Registration Success"}')
 
+    def test_login_success(self):
+        # Register user
+        self.client.post('/api/register', {'email': 'test1000@email.com', 'username': 'test1000', 'password': 'test1000'}, format='json')
+
+        response = self.client.post('/api/login', {'username': 'test1000', 'password': 'test1000'}, format='json')
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertIn(b'"auth": true', response.content)
+
     def test_login_failure(self):
-        client = APIClient()
-        response = client.post('/api/login', {'username': 'test1000', 'password': 'test1000'}, format='json')
+        response = self.client.post('/api/login', {'username': 'test1000', 'password': 'test1000'}, format='json')
         self.assertEquals(response.status_code, status.HTTP_200_OK)
         self.assertEquals(response.content, b'{"auth": false, "err_msg": "User does not exist"}')
