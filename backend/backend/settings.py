b673798160322fc03a525ab0d9c869eef779499f
Salmaan Saeed <salsaeed999@gmail.com>
2023-09-02 00:51:52 -0400

    django uses sqlite lite in ci/cd

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -98,6 +98,9 @@ if 'test' in sys.argv or 'test_coverage' in sys.argv:
     # Covers regular testing and django-coverage
     DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
+if os.environ.get('CI'):
+    # Enable using SQLite in CI/CD
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
