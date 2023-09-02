a020243876f3921a95412179d55b4a6b09a8ad7d
Salmaan Saeed <salsaeed999@gmail.com>
2023-09-01 23:34:58 -0400

    wrote tests for login and register

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from rest_framework import status
 
 # Create your tests here.
+
+from rest_framework.test import APIClient
+
+
+class Auth(TestCase):
+    def test_register_success(self):
+        client = APIClient()
+        response = client.post('/api/register', {'email': 'test1000@email.com', 'username': 'test1000', 'password': 'test1000'}, format='json')
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEquals(response.content, b'{"registered": true, "success_msg": "User Registration Success"}')
+
+    def test_login_failure(self):
+        client = APIClient()
+        response = client.post('/api/login', {'username': 'test1000', 'password': 'test1000'}, format='json')
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEquals(response.content, b'{"auth": false, "err_msg": "User does not exist"}')
