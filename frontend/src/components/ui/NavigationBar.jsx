6d66653caabecb354be0589b79b5c0fde0bb3e61
Salmaan Saeed <salsaeed999@gmail.com>
2023-09-03 14:21:45 -0400

    use single quotes for importing css

--- frontend/src/components/ui/NavigationBar.jsx
+++ frontend/src/components/ui/NavigationBar.jsx
@@ -4,7 +4,7 @@ import { Nav } from 'react-bootstrap'
 import { Navbar } from 'react-bootstrap'
 import { Logout } from '../auth/logout'
 
-import "./NavigationBar.css"
+import './NavigationBar.css'
 
 export default function NavigationBar({isAuth}) {
     return (
