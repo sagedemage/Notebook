08a307f0872a27a488b1914a6f228f91f3fb4c1c
Salmaan Saeed <salsaeed999@gmail.com>
2024-01-08 18:44:58 -0500

    The AuthRoute function should only return the outlet and changed the format of this code to comply with google javascript style guide

--- frontend/src/components/auth/auth_route.jsx
+++ frontend/src/components/auth/auth_route.jsx
@@ -1,17 +1,17 @@
 
-import { Navigate, Outlet, useLocation } from 'react-router-dom'
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
 
-export default function AuthRoute({isAuth}) {
-    const origin = useLocation()
+export default function AuthRoute({ isAuth }) {
+  const origin = useLocation();
 
-    if (isAuth !== true) {
-        return (
-            <div>
-                <h1> Unathorized User </h1>
-                <p> The User is Unauthorized. Click <a href="/">here</a> to go back</p>
-            </div>
-        );
-    }
+  if (isAuth === false) {
+    return (
+      <div>
+        <h1> Unathorized User </h1>
+        <p> The User is Unauthorized. Click <a href="/">here</a> to go back</p>
+      </div>
+    );
+  }
 
-    return isAuth ? <Outlet /> : <Navigate to="/" replace state={{ from: origin}} />
+  return <Outlet />;
 }
