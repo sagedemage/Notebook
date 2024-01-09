a954bda8b05d7b23f35d66ebc4240ee442b51c97
Salmaan Saeed <salsaeed999@gmail.com>
2024-01-08 19:26:27 -0500

    configured eslint to enforce the Google JavaScript style guide

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'google'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
@@ -32,5 +33,8 @@ module.exports = {
     ],
     'react/prop-types': 'off',
     'react/no-unknown-property': 'off',
+    'object-curly-spacing': 'off',
+    'require-jsdoc': 'off',
+    'valid-jsdoc': 'off',
   },
 }
