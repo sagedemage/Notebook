db72eb25cd01e9beeb26953c5fbd06e3581d296d
Salmaan Saeed <salsaeed999@gmail.com>
2024-01-09 18:35:08 -0500

    Removed the google extension from the ESLint config. Added the @stylistic/eslint-plugin plugin, and the rules that comply with my modifed version of the Google style guide in the ESLint config.

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -6,22 +6,112 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
-    'google',
-    'plugin:jsdoc/recommended-error'
+    'plugin:jsdoc/recommended-error',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react', 'react-refresh'],
+  plugins: ['react', 'react-refresh', '@stylistic/eslint-plugin'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
     'eqeqeq': 'error',
-    'curly': 'error',
-    'object-curly-spacing': 'off',
-    'require-jsdoc': 'off',
-    'valid-jsdoc': 'off',
+
+    /*
+      Google Style Guide
+      Got a good chunk of the rules from google/eslint-config-google repository.
+      link: https://github.com/google/eslint-config-google/blob/master/index.js
+    */
+
+    // Best Practices
+    'curly': ['error', 'multi-line'],
+    'guard-for-in': 'error',
+    'no-caller': 'error',
+    'no-extend-native': 'error',
+    'no-extra-bind': 'error',
+    'no-invalid-this': 'error',
+    'no-multi-str': 'error',
+    'no-new-wrappers': 'error',
+    'prefer-promise-reject-errors': 'error',
+
+    // Stylistic Issues
+    'camelcase': ['error', { properties: 'never' }],
+    'new-cap': 'error',
+    'no-array-constructor': 'error',
+    'no-new-object': 'error',
+    'one-var': ['error', {
+      var: 'never',
+      let: 'never',
+      const: 'never',
+    }],
+    '@stylistic/arrow-parens': 'error',
+    '@stylistic/no-multi-spaces': 'error',
+    '@stylistic/array-bracket-spacing': 'error',
+    '@stylistic/block-spacing': 'error',
+    '@stylistic/brace-style': 'error',
+    '@stylistic/comma-dangle': ['error', 'always-multiline'],
+    '@stylistic/comma-spacing': 'error',
+    '@stylistic/computed-property-spacing': 'error',
+    '@stylistic/comma-style': 'error',
+    '@stylistic/eol-last': 'error',
+    '@stylistic/func-call-spacing': 'error',
+    '@stylistic/indent': [
+      'error', 2, {
+        'CallExpression': {
+          'arguments': 2,
+        },
+        'FunctionDeclaration': {
+          'body': 1,
+          'parameters': 2,
+        },
+        'FunctionExpression': {
+          'body': 1,
+          'parameters': 2,
+        },
+        'MemberExpression': 2,
+        'ObjectExpression': 1,
+        'SwitchCase': 1,
+        'ignoredNodes': [
+          'ConditionalExpression',
+        ],
+      },
+    ],
+    '@stylistic/max-len': ['error', {
+      code: 80,
+      tabWidth: 2,
+      ignoreUrls: true,
+      ignorePattern: 'goog\.(module|require)',
+    }],
+    '@stylistic/key-spacing': 'error',
+    '@stylistic/keyword-spacing': 'error',
+    '@stylistic/linebreak-style': 'error',
+    '@stylistic/no-multiple-empty-lines': ['error', { max: 2 }],
+    '@stylistic/no-tabs': 'error',
+    '@stylistic/no-trailing-spaces': 'error',
+    '@stylistic/operator-linebreak': ['error', 'after'],
+    '@stylistic/padded-blocks': ['error', 'never'],
+    '@stylistic/quote-props': ['error', 'consistent'],
+    '@stylistic/quotes': ['error', 'single', { allowTemplateLiterals: true }],
+    '@stylistic/semi': 'error',
+    '@stylistic/semi-spacing': 'error',
+    '@stylistic/space-before-blocks': 'error',
+    '@stylistic/space-before-function-paren': ['error', {
+      asyncArrow: 'always',
+      anonymous: 'never',
+      named: 'never',
+    }],
+    '@stylistic/spaced-comment': ['error', 'always'],
+    '@stylistic/switch-colon-spacing': 'error',
+    '@stylistic/generator-star-spacing': ['error', 'after'],
+    '@stylistic/rest-spread-spacing': 'error',
+    '@stylistic/yield-star-spacing': ['error', 'after'],
+
+    // ECMAScript 6
+    'no-var': 'error',
+    'prefer-const': ['error', { destructuring: 'all' }],
+    'prefer-rest-params': 'error',
+    'prefer-spread': 'error',
   },
 }
