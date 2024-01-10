2cd9cb35183e507ce56fe08a8a6cf878330c72ed
Salmaan Saeed <salsaeed999@gmail.com>
2024-01-10 18:46:46 -0500

    fixed the format of the back-end codebase so that it complies with the Google Python style guide

--- backend/api/views/auth.py
+++ backend/api/views/auth.py
@@ -12,7 +12,7 @@ from ..models import User
 
 @csrf_exempt
 @api_view(['POST'])
-def register(request):  # pylint: disable=unused-variable
+def register(request):
     """
     Register a new user
 
@@ -22,30 +22,43 @@ def register(request):  # pylint: disable=unused-variable
     - username: string
     - password: string
     """
-    response = JsonResponse({'err_msg': "Data not valid"})
+    response = JsonResponse({'err_msg': 'Data not valid'})
 
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
-        email_match = User.objects.filter(email__exact=serializer.data.get("email"))
-        username_match = User.objects.filter(username__exact=serializer.data.get("username"))
-        password = serializer.data.get("password")
+        email_match = User.objects.filter(
+            email__exact=serializer.data.get('email')
+        )
+        username_match = User.objects.filter(
+            username__exact=serializer.data.get('username')
+        )
+        password = serializer.data.get('password')
         if email_match.exists() is True:
-            response = JsonResponse({'registered': False, 'err_msg': "Email already exists!"})
+            response = JsonResponse(
+                {'registered': False, 'err_msg': 'Email already exists!'}
+            )
         elif username_match.exists() is True:
-            response = JsonResponse({'registered': False, 'err_msg': "Username already exists!"})
+            response = JsonResponse(
+                {'registered': False, 'err_msg': 'Username already exists!'}
+            )
         else:
             hashed_password = make_password(password)
-            user = User(email=serializer.data.get("email"), username=serializer.data.get("username"),
-                        password=hashed_password)
+            user = User(
+                email=serializer.data.get('email'),
+                username=serializer.data.get('username'),
+                password=hashed_password
+            )
             user.save()
-            response = JsonResponse({'registered': True, 'success_msg': "User Registration Success"})
+            response = JsonResponse(
+                {'registered': True, 'success_msg': 'User Registration Success'}
+            )
 
     return response
 
 
 @csrf_exempt
 @api_view(['POST'])
-def login(request):  # pylint: disable=unused-variable
+def login(request):
     """
     Login a user
 
@@ -54,22 +67,39 @@ def login(request):  # pylint: disable=unused-variable
     - username: string
     - password: string
     """
-    email_match = User.objects.filter(email__exact=request.data.get("username"))
-    username_match = User.objects.filter(username__exact=request.data.get("username"))
-    entered_password = request.data.get("password")
+    email_match = User.objects.filter(email__exact=request.data.get('username'))
+    username_match = User.objects.filter(
+        username__exact=request.data.get('username')
+    )
+    entered_password = request.data.get('password')
     if email_match.exists():
         actual_password = email_match.values_list('password', flat=True).get()
         password_match = check_password(entered_password, actual_password)
         if password_match:
             user_id = email_match.values_list('id', flat=True).get()
             token = generate_token(user_id)
-            return JsonResponse({'auth': True, 'token': token, 'success_msg': "Successful Login"})
+            return JsonResponse(
+                {
+                    'auth': True,
+                    'token': token,
+                    'success_msg': 'Successful Login'
+                }
+            )
     elif username_match.exists():
-        actual_password = username_match.values_list('password', flat=True).get()
+        actual_password = username_match.values_list(
+            'password',
+            flat=True
+        ).get()
         password_match = check_password(entered_password, actual_password)
         if password_match:
             user_id = username_match.values_list('id', flat=True).get()
             token = generate_token(user_id)
-            return JsonResponse({'auth': True, 'token': token, 'success_msg': "Successful Login"})
+            return JsonResponse(
+                {
+                    'auth': True,
+                    'token': token,
+                    'success_msg': 'Successful Login'
+                }
+            )
 
-    return JsonResponse({'auth': False, 'err_msg': "Failed to Login"})
+    return JsonResponse({'auth': False, 'err_msg': 'Failed to Login'})
--- backend/api/views/note.py
+++ backend/api/views/note.py
@@ -2,7 +2,7 @@
 
 from django.http import HttpResponse
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt;
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from ..serializers import NoteSerializer
 from ..models import Note, User
@@ -10,7 +10,7 @@ from ..models import Note, User
 
 @csrf_exempt
 @api_view(['POST'])
-def add_note(request):  # pylint: disable=unused-variable
+def add_note(request):
     """
     Add a note and store the user_id associated with the note
     Request Parameters:
@@ -21,19 +21,19 @@ def add_note(request):  # pylint: disable=unused-variable
     serializer = NoteSerializer(data=request.data)
     if serializer.is_valid():
         note = Note(
-            title=request.data.get("title"),
-            description=request.data.get("description"),
-            user_id=request.data.get("user_id"),
+            title=request.data.get('title'),
+            description=request.data.get('description'),
+            user_id=request.data.get('user_id'),
         )
         note.save()
-        return HttpResponse("Add Note")
+        return HttpResponse('Add Note')
 
-    return HttpResponse("Data not valid")
+    return HttpResponse('Data not valid')
 
 
 @csrf_exempt
 @api_view(['PATCH'])
-def edit_note(request):  # pylint: disable=unused-variable
+def edit_note(request):
     """
     Update a note by id (Note id)
     Request Parameters:
@@ -43,51 +43,51 @@ def edit_note(request):  # pylint: disable=unused-variable
     """
     serializer = NoteSerializer(data=request.data)
     if serializer.is_valid():
-        note_id = request.data.get("note_id")
+        note_id = request.data.get('note_id')
         note = Note.objects.get(id=note_id)
-        note.title = request.data.get("title")
-        note.description = request.data.get("description")
+        note.title = request.data.get('title')
+        note.description = request.data.get('description')
         note.save()
-        return HttpResponse("Update Note")
+        return HttpResponse('Update Note')
 
-    return HttpResponse("Data not valid")
+    return HttpResponse('Data not valid')
 
 
 @csrf_exempt
 @api_view(['DELETE'])
-def delete_note(request):  # pylint: disable=unused-variable
+def delete_note(request):
     """
     Delete a note by id (Note id)
     Request Parameters:
     - note_id: integer
     """
-    note_id = request.data.get("note_id")
+    note_id = request.data.get('note_id')
     note = Note.objects.filter(id=note_id)
     note.delete()
-    return HttpResponse("Delete Note")
+    return HttpResponse('Delete Note')
 
 
 @csrf_exempt
 @api_view(['GET'])
-def view_notes(request):  # pylint: disable=unused-variable
+def view_notes(request):
     """
     Return the notes the user owns
     Request Parameters:
     - note_id: integer
     """
-    user_id = request.GET.get("user_id", "")
+    user_id = request.GET.get('user_id', '')
     user = User.objects.filter(id=user_id)
     if user.exists():
         notes = Note.objects.filter(user_id__exact=user_id)
         serializer = NoteSerializer(notes, many=True)
         return JsonResponse({'notes': serializer.data})
 
-    return HttpResponse("User does not exist")
+    return HttpResponse('User does not exist')
 
 
 @csrf_exempt
 @api_view(['GET'])
-def fetch_note(request):  # pylint: disable=unused-variable
+def fetch_note(request):
     """
     Fetch a note by id (Note id)
     Route: /api/fetch-note?user_id={number}
@@ -97,4 +97,4 @@ def fetch_note(request):  # pylint: disable=unused-variable
     """
     note_id = request.GET.get('id', '')
     note = Note.objects.get(id=note_id)
-    return JsonResponse({"title": note.title, "description": note.description})
+    return JsonResponse({'title': note.title, 'description': note.description})
