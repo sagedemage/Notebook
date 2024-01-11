028218eb6e4a96e79ddd4e4cc2f21666c7941f28
Salmaan Saeed <salsaeed999@gmail.com>
2024-01-10 23:33:01 -0500

    add one line to disable line too long (pylint message) in the settings.py file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+# pylint: disable=line-too-long
+
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -25,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ak@7#x56##0t=tsapst1h1$w=85w-nkjw%@d-++)0i3=cn!7xd'  # pylint: disable=line-too-long
+SECRET_KEY = 'django-insecure-ak@7#x56##0t=tsapst1h1$w=85w-nkjw%@d-++)0i3=cn!7xd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -105,16 +107,16 @@ if os.environ.get('CI'):
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',  # pylint: disable=line-too-long
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',  # pylint: disable=line-too-long
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',  # pylint: disable=line-too-long
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',  # pylint: disable=line-too-long
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
